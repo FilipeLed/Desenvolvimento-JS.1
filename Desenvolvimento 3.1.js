ca4811ab866d10012431ea10ef3f8ee6da8632e3
Filipe Liberato Justino Diniz <filipe.led@gmail.com>
2023-09-24 20:53:11 -0300

    ...

---
+++ Desenvolvimento 3.1.js
@@ -0,0 +1,13 @@
+// Exemplo de cadastro de pessoa
+
+// Definir variáveis para cada campo
+var nome = "Filipe da Silva"; // string
+var salario = 2500.50; // número (ponto flutuante)
+var idade = 33; // número (inteiro)
+var possuiDiploma = true; // booleano
+
+// Exibir os valores no console
+console.log("Nome: " + nome);
+console.log("Salário: R$" + salario.toFixed(2)); // Formatando para exibir duas casas decimais
+console.log("Idade: " + idade + " anos");
+console.log("Possui Diploma: " + (possuiDiploma ? "Sim" : "Não"));
